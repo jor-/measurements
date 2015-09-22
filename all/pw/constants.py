05bc3e118e49c4eb8cb3ab54fad7656210b07997
Joscha Reimer <jor@informatik.uni-kiel.de>
2015-09-22 14:37:47 +0200

    MAINT: remove uncommented code

--- all/pw/constants.py
+++ all/pw/constants.py
@@ -4,12 +4,6 @@ from measurements.all.constants import BASE_DIR
 
 BASE_DIR = os.path.join(BASE_DIR, 'pw')
 CORRELATION_DIR = os.path.join(BASE_DIR, 'correlation')
-# MATRIX_FILENAME_PREFIX = 'matrix.min_{min_values:0>2}_measurements.max_{max_year_diff:0>2}_year_diff.positive_definite.'
-# MATRIX_FILENAME =  'matrix.min_{min_values:0>2}_measurements.max_{max_year_diff:0>2}_year_diff.positive_definite.{type}.ppy'
-# DOK_MATRIX_FILENAME =  MATRIX_FILENAME_PREFIX + 'dok.ppy'
-# CSR_MATRIX_FILENAME =  MATRIX_FILENAME_PREFIX + 'csr.ppy'
-# CHOLESKY_L_MATRIX_FILENAME =  MATRIX_FILENAME_PREFIX + 'L.csc.ppy'
-# CHOLESKY_P_MATRIX_FILENAME =  MATRIX_FILENAME_PREFIX + 'P.csc.ppy'
 
 
 POINT_INDEX_FILENAME = 'point_index_{}_dict.ppy'
