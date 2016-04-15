9a6d70f138ea3adc0fe5ac25638dc498a040f1b6
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-04-15 16:27:46 +0200

    ENH: dop.pw.deviation: total deviation calculated with noise ratio added

--- dop/pw/deviation.py
+++ dop/pw/deviation.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+import measurements.dop.constants
 import measurements.dop.pw.constants
 import measurements.dop.pw.data
 import measurements.land_sea_mask.data
@@ -8,50 +9,46 @@ import util.logging
 logger = util.logging.logger
 
 
-
-def average_noise(sample_lsm=measurements.dop.pw.constants.SAMPLE_LSM):
-    from measurements.dop.constants import DEVIATION_MIN_MEASUREMENTS, DEVIATION_MIN_VALUE
-
+def average_noise_deviation(sample_lsm=measurements.dop.pw.constants.SAMPLE_LSM):
     ## calculate deviations
     m = measurements.dop.pw.data.measurement_dict()
     m.categorize_indices_to_lsm(sample_lsm, discard_year=False)
-    deviations = m.deviations(min_values=DEVIATION_MIN_MEASUREMENTS)
+    deviations = m.deviations(min_values=measurements.dop.constants.DEVIATION_MIN_MEASUREMENTS)
     
     if len(deviations) == 0:
         raise ValueError('No deviation available from data with smaple lsm {}.'.format(sample_lsm))
 
     ## apply lower value bound
-    deviations[deviations[:, -1] < DEVIATION_MIN_VALUE, -1] = DEVIATION_MIN_VALUE
+    deviations[deviations[:, -1] < measurements.dop.constants.DEVIATION_MIN_VALUE, -1] = measurements.dop.constants.DEVIATION_MIN_VALUE
 
     ## average
     average_deviation = np.mean(deviations[:, -1])
-    logger.debug('Got averaged noise DOP deviation {} from {} estimations.'.format(average_deviation, len(deviations)))
+    logger.debug('Got average DOP noise deviation {} from {} estimations.'.format(average_deviation, len(deviations)))
 
     return average_deviation
 
 
+def average_concentration_deviation(sample_lsm=measurements.dop.pw.constants.SAMPLE_LSM):
+    average_concentration_deviation = average_noise_deviation(sample_lsm=sample_lsm) * measurements.dop.pw.constants.DEVIATION_CONCENTRATION_NOISE_RATIO
+    logger.debug('Got average DOP concentration deviation {} from concentration noise ratio {}.'.format(average_concentration_deviation, measurements.dop.pw.constants.DEVIATION_CONCENTRATION_NOISE_RATIO))
+    return average_concentration_deviation
+
+
 def average_total_deviation(sample_lsm=measurements.dop.pw.constants.SAMPLE_LSM):
-    return average_noise(sample_lsm=sample_lsm)
+    average_total_deviation = (average_noise_deviation(sample_lsm=sample_lsm)**2 + average_concentration_deviation(sample_lsm=sample_lsm)**2 )**(1/2)
+    logger.debug('Got average DOP total deviation {}.'.format(average_total_deviation))
+    return average_total_deviation
 
 
-def noise_deviation_for_points(points=None, sample_lsm=measurements.dop.pw.constants.SAMPLE_LSM):
-    from measurements.dop.constants import DEVIATION_MIN_MEASUREMENTS, DEVIATION_MIN_VALUE
-
+def total_deviation_for_points(points=None, sample_lsm=measurements.dop.pw.constants.SAMPLE_LSM):
     ## load points if not passed
     if points is None:
         points = measurements.dop.pw.data.points_and_results()[0]
 
-    ## calculate average deviation
-    average_noise_deviation = average_noise(sample_lsm=sample_lsm)
+    ## calculate total deviation
+    total_deviation = np.ones(len(points)) * average_total_deviation(sample_lsm=sample_lsm)
 
     ## return deviation
-    noise_deviation = np.ones(len(points)) * average_noise_deviation
-
-    return noise_deviation
-
-
-
-def total_deviation_for_points(points=None, sample_lsm=measurements.dop.pw.constants.SAMPLE_LSM):
-    return noise_deviation_for_points(points=points, sample_lsm=sample_lsm)
+    return total_deviation
 
 
