9389eb4de6e71b7ec5fdbc2f01d4850d70c48aa9
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-04-15 16:29:04 +0200

    MAINT: po4.wod.plot: some old plotting scripts removed

--- po4/wod/plot/interpolated_deviation_histogram.py
+++
@@ -1,5 +0,0 @@
-from util.logging import Logger
-from measurements.po4.wod.plot.interface import plot_interpolated_deviation_histogram
-
-with Logger():
-    plot_interpolated_deviation_histogram(x_min=0, x_max=2)
--- po4/wod/plot/sample_mean_histogram.py
+++
@@ -1,6 +0,0 @@
-from util.logging import Logger
-from measurements.po4.wod.plot.interface import plot_mean_histogram
-
-
-with Logger():
-    plot_mean_histogram(x_min=0, x_max=15)
