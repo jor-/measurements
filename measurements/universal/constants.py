f81189522086372543e1cfba9e371e93a28b0add
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-10-26 14:28:42 +0200

    MAINT: measurements.universal.constants: filenames as seperate constants removed

--- measurements/universal/constants.py
+++ measurements/universal/constants.py
@@ -29,8 +29,8 @@ MEAN_MIN_MEASUREMENTS = 2
 
 MEAN_DIR = os.path.join(MEASUREMENT_DIR, 'mean')
 
-MEAN_FILENAME = 'concentration_mean_-_for_{target}_-_sample_{sample_lsm}_-_min_measurements_{min_measurements:d}_-_fill_{fill_strategy}.npy'
-MEAN_FILE = os.path.join(MEAN_DIR, MEAN_FILENAME)
+MEAN_FILE = os.path.join(MEAN_DIR,
+'concentration_mean_-_for_{target}_-_sample_{sample_lsm}_-_min_measurements_{min_measurements:d}_-_fill_{fill_strategy}.npy')
 
 
 ## deviation
@@ -39,8 +39,8 @@ DEVIATION_MIN_MEASUREMENTS = 3
 
 DEVIATION_DIR = os.path.join(MEASUREMENT_DIR, 'deviation')
 
-DEVIATION_FILENAME = '{deviation_type}_-_for_{target}_-_sample_{sample_lsm}_-_min_measurements_{min_measurements:d}_-_min_deviation_{min_standard_deviation:g}_-_fill_{fill_strategy}.npy'
-DEVIATION_FILE = os.path.join(DEVIATION_DIR, DEVIATION_FILENAME)
+DEVIATION_FILE = os.path.join(DEVIATION_DIR,
+'{deviation_type}_-_for_{target}_-_sample_{sample_lsm}_-_min_measurements_{min_measurements:d}_-_min_deviation_{min_standard_deviation:g}_-_fill_{fill_strategy}.npy')
 
 
 ## correlation
@@ -59,35 +59,35 @@ CORRELATION_FORMAT = 'csc'
 CORRELATION_DIR = os.path.join(MEASUREMENT_DIR, 'correlation')
 
 
-MAP_INDEX_TO_POINT_INDEX_DICT_FILENAME = 'map_indices_to_point_index_dict_-_sample_{sample_lsm}_-_year_discarded_{discard_year}.ppy'
-MAP_INDEX_TO_POINT_INDEX_DICT_FILE = os.path.join(CORRELATION_DIR, MAP_INDEX_TO_POINT_INDEX_DICT_FILENAME)
+MAP_INDEX_TO_POINT_INDEX_DICT_FILE = os.path.join(CORRELATION_DIR,
+'map_indices_to_point_index_dict_-_sample_{sample_lsm}_-_year_discarded_{discard_year}.ppy')
 
-CONCENTRATIONS_SAME_POINTS_EXCEPT_YEAR_DICT_FILENAME = 'concentrations_same_points_except_year_dict_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}.ppy'
-CONCENTRATIONS_SAME_POINTS_EXCEPT_YEAR_DICT_FILE = os.path.join(CORRELATION_DIR, CONCENTRATIONS_SAME_POINTS_EXCEPT_YEAR_DICT_FILENAME)
+CONCENTRATIONS_SAME_POINTS_EXCEPT_YEAR_DICT_FILE = os.path.join(CORRELATION_DIR,
+'concentrations_same_points_except_year_dict_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}.ppy')
 
-SAMPLE_COVARIANCE_DICT_FILENAME = 'sample_covariance_dict.nonstationary_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_max_year_diff_{max_year_diff:0>2}.ppy'
-SAMPLE_COVARIANCE_DICT_FILE = os.path.join(CORRELATION_DIR, SAMPLE_COVARIANCE_DICT_FILENAME)
+SAMPLE_COVARIANCE_DICT_FILE = os.path.join(CORRELATION_DIR,
+'sample_covariance_dict.nonstationary_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_max_year_diff_{max_year_diff:0>2}.ppy')
 
 DEVIATION_DESCRIPTION = 'min_values_{min_measurements_standard_deviation:d}_-_min_{min_standard_deviation:g}_-_fill_{fill_strategy}'
 
-SAMPLE_CORRELATION_MATRIX_SAME_BOX_LOWER_TRIANGLE_MATRIX_FILENAME = 'sample_correlation.same_box.lower_triangle_-_sample_{sample_lsm}_-_min_abs_{min_abs_correlation}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz'
-SAMPLE_CORRELATION_MATRIX_SAME_BOX_LOWER_TRIANGLE_MATRIX_FILE = os.path.join(CORRELATION_DIR, SAMPLE_CORRELATION_MATRIX_SAME_BOX_LOWER_TRIANGLE_MATRIX_FILENAME)
+SAMPLE_CORRELATION_MATRIX_SAME_BOX_LOWER_TRIANGLE_MATRIX_FILE = os.path.join(CORRELATION_DIR,
+'sample_correlation.same_box.lower_triangle_-_sample_{sample_lsm}_-_min_abs_{min_abs_correlation}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz')
 
-SAMPLE_QUANTITY_MATRIX_DIFFERENT_BOXES_LOWER_TRIANGLE_MATRIX_FILENAME = 'sample_quantity.different_boxes.lower_triangle_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_max_year_diff_{max_year_diff:0>2}_-_min_abs_{min_abs_correlation}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz'
-SAMPLE_QUANTITY_MATRIX_DIFFERENT_BOXES_LOWER_TRIANGLE_MATRIX_FILE = os.path.join(CORRELATION_DIR, SAMPLE_QUANTITY_MATRIX_DIFFERENT_BOXES_LOWER_TRIANGLE_MATRIX_FILENAME)
+SAMPLE_QUANTITY_MATRIX_DIFFERENT_BOXES_LOWER_TRIANGLE_MATRIX_FILE = os.path.join(CORRELATION_DIR,
+'sample_quantity.different_boxes.lower_triangle_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_max_year_diff_{max_year_diff:0>2}_-_min_abs_{min_abs_correlation}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz')
 
-SAMPLE_CORRELATION_MATRIX_DIFFERENT_BOXES_LOWER_TRIANGLE_MATRIX_FILENAME = 'sample_correlation.different_boxes.lower_triangle_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_max_year_diff_{max_year_diff:0>2}_-_min_abs_{min_abs_correlation}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz'
-SAMPLE_CORRELATION_MATRIX_DIFFERENT_BOXES_LOWER_TRIANGLE_MATRIX_FILE = os.path.join(CORRELATION_DIR, SAMPLE_CORRELATION_MATRIX_DIFFERENT_BOXES_LOWER_TRIANGLE_MATRIX_FILENAME)
+SAMPLE_CORRELATION_MATRIX_DIFFERENT_BOXES_LOWER_TRIANGLE_MATRIX_FILE = os.path.join(CORRELATION_DIR,
+'sample_correlation.different_boxes.lower_triangle_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_max_year_diff_{max_year_diff:0>2}_-_min_abs_{min_abs_correlation}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz')
 
-SAMPLE_CORRELATION_MATRIX_FILENAME = 'sample_correlation_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_min_abs_{min_abs_correlation}_-_max_abs_{max_abs_correlation}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz'
-SAMPLE_CORRELATION_MATRIX_FILE = os.path.join(CORRELATION_DIR, SAMPLE_CORRELATION_MATRIX_FILENAME)
+SAMPLE_CORRELATION_MATRIX_FILE = os.path.join(CORRELATION_DIR,
+'sample_correlation_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_min_abs_{min_abs_correlation}_-_max_abs_{max_abs_correlation}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz')
 
-CORRELATION_MATRIX_POSITIVE_DEFINITE_FILENAME = 'correlation.positive_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_min_abs_{min_abs_correlation}_-_max_abs_{max_abs_correlation}_-_{ordering_method}_ordering.reordering_{reordering}_-_min_diag_{cholesky_min_diag_value:.0e}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz'
-CORRELATION_MATRIX_POSITIVE_DEFINITE_FILE = os.path.join(CORRELATION_DIR, CORRELATION_MATRIX_POSITIVE_DEFINITE_FILENAME)
+CORRELATION_MATRIX_POSITIVE_DEFINITE_FILE = os.path.join(CORRELATION_DIR,
+'correlation.positive_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_min_abs_{min_abs_correlation}_-_max_abs_{max_abs_correlation}_-_{ordering_method}_ordering.reordering_{reordering}_-_min_diag_{cholesky_min_diag_value:.0e}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz')
 
-CORRELATION_MATRIX_POSITIVE_DEFINITE_REDUCTION_FACTORS_FILENAME = 'correlation.positive.reduction_factors_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_min_abs_{min_abs_correlation}_-_max_abs_{max_abs_correlation}_-_{ordering_method}_ordering.reordering_{reordering}_-_min_diag_{cholesky_min_diag_value:.0e}_-_deviation_{standard_deviation_description}.npy'
-CORRELATION_MATRIX_POSITIVE_DEFINITE_REDUCTION_FACTORS_FILE = os.path.join(CORRELATION_DIR, CORRELATION_MATRIX_POSITIVE_DEFINITE_REDUCTION_FACTORS_FILENAME)
+CORRELATION_MATRIX_POSITIVE_DEFINITE_REDUCTION_FACTORS_FILE = os.path.join(CORRELATION_DIR,
+'correlation.positive.reduction_factors_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_min_abs_{min_abs_correlation}_-_max_abs_{max_abs_correlation}_-_{ordering_method}_ordering.reordering_{reordering}_-_min_diag_{cholesky_min_diag_value:.0e}_-_deviation_{standard_deviation_description}.npy')
 
-CORRELATION_MATRIX_CHOLESKY_FACTOR_FILENAME = 'correlation.positive.cholesky_{factor_type}_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_min_abs_{min_abs_correlation}_-_max_abs_{max_abs_correlation}_-_{ordering_method}_ordering.reordering_{reordering}_-_min_diag_{cholesky_min_diag_value:.0e}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz'
-CORRELATION_MATRIX_CHOLESKY_FACTOR_FILE = os.path.join(CORRELATION_DIR, CORRELATION_MATRIX_CHOLESKY_FACTOR_FILENAME)
+CORRELATION_MATRIX_CHOLESKY_FACTOR_FILE = os.path.join(CORRELATION_DIR,
+'correlation.positive.cholesky_{factor_type}_-_sample_{sample_lsm}_-_min_values_{min_measurements_correlation:0>2d}_-_min_abs_{min_abs_correlation}_-_max_abs_{max_abs_correlation}_-_{ordering_method}_ordering.reordering_{reordering}_-_min_diag_{cholesky_min_diag_value:.0e}_-_deviation_{standard_deviation_description}_-_{dtype}.{matrix_format}.npz')
 
