04d5897159d6e17ae19365cfe95e65de4bde789c
Joscha Reimer <jor@informatik.uni-kiel.de>
2017-09-29 11:50:01 +0200

    ENH: measurements.dop: updated to new API of measurements.universal

--- measurements/dop/constants.py
+++ measurements/dop/constants.py
@@ -1,5 +1,36 @@
-from measurements.constants import DEVIATION_MIN_MEASUREMENTS, DEVIATION_MIN_VALUE, MEAN_MIN_MEASUREMENTS, CORRELATION_MIN_ABS_VALUE, CORRELATION_MAX_ABS_VALUE
+import os.path
+import datetime
 
-T_RANGE = [0, 1]
-X_RANGE = [0, 360]
-Y_RANGE = [-90, 90]
+from measurements.constants import BASE_DIR
+
+
+# data information
+DATA_DIR = os.path.join(BASE_DIR, 'dop', 'data', 'raw')
+
+LADOLFI_2002_DIR = os.path.join(DATA_DIR, 'Ladolfi2002')
+LADOLFI_2002_MEASUREMENT_FILE = os.path.join(LADOLFI_2002_DIR, 'CD139_DOP_prepared.txt')
+LADOLFI_2002_START_DATE = datetime.date(2002, 3, 1)
+LADOLFI_2002_END_DATE = datetime.date(2002, 4, 15)
+LADOLFI_2002_VALID_DATA_FLAG = 1
+
+LADOLFI_2004_DIR = os.path.join(DATA_DIR, 'Ladolfi2004')
+LADOLFI_2004_MEASUREMENT_FILE = os.path.join(LADOLFI_2004_DIR, 'D279_DOP_prepared.txt')
+LADOLFI_2004_START_DATE = datetime.date(2004, 4, 4)
+LADOLFI_2004_END_DATE = datetime.date(2004, 5, 10)
+LADOLFI_2004_VALID_DATA_FLAG = 0
+
+YOSHIMURA_2007_DIR = os.path.join(DATA_DIR, 'Yoshimura2007')
+YOSHIMURA_2007_MEASUREMENT_FILE = os.path.join(YOSHIMURA_2007_DIR, 'Yoshimura2007.txt')
+
+DATA_FILENAME = 'data.npy'
+
+
+# sample informations
+
+from measurements.universal.constants import SAMPLE_LSM
+
+STANDARD_DEVIATION_CONCENTRATION_NOISE_RATIO = 1.5
+
+STANDARD_DEVIATION_MIN_VALUE = 0.5
+
+CORRELATION_MIN_MEASUREMENTS = 40
