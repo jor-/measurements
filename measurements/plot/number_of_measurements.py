94816b11f68162c0a78ca37da15d6eee04053d93
Joscha Reimer <jor@informatik.uni-kiel.de>
2018-11-13 15:51:41 +0100

    ENH: measurements.plot.number_of_measurements: plots for number of measurements added

---
+++ measurements/plot/number_of_measurements.py
@@ -0,0 +1,99 @@
+import os.path
+
+import numpy as np
+
+import util.plot
+import measurements.plot.constants
+
+
+def per_time(measurements_object, step_size=None, file=None, overwrite=False):
+    if step_size is None:
+        step_size = 1
+
+    if file is None:
+        file = measurements.plot.constants.PLOT_FILE.format(
+            tracer=measurements_object.tracer,
+            data_set=measurements_object.data_set_name,
+            kind='number_of_measurements',
+            kind_id=str(measurements_object.sample_lsm),
+            plot_name=f'number_of_measurements_per_time_-_step_size_{step_size}')
+
+    points = measurements_object.points
+    assert points.ndim == 2
+    assert points.shape[1] == 4
+    t = points[:, 0]
+
+    util.plot.histogram(file, t, step_size=step_size, overwrite=overwrite)
+
+
+def per_year(measurements_object, number_of_bins=None, file=None, overwrite=False):
+    if number_of_bins is None:
+        number_of_bins = 365
+    else:
+        assert int(number_of_bins) == number_of_bins
+        number_of_bins = int(number_of_bins)
+    step_size = 1. / number_of_bins
+
+    if file is None:
+        file = measurements.plot.constants.PLOT_FILE.format(
+            tracer=measurements_object.tracer,
+            data_set=measurements_object.data_set_name,
+            kind='number_of_measurements',
+            kind_id=str(measurements_object.sample_lsm),
+            plot_name=f'number_of_measurements_within_a_year_-_number_of_bins_{number_of_bins}')
+
+    points = measurements_object.points
+    assert points.ndim == 2
+    assert points.shape[1] == 4
+    t = points[:, 0]
+    t = t % 1
+
+    util.plot.histogram(file, t, step_size=step_size, overwrite=overwrite)
+
+
+def per_depth(measurements_object, step_size=None, use_log_scale=True, file=None, overwrite=False):
+    if step_size is None:
+        step_size = 50.
+
+    if file is None:
+        plot_name = f'number_of_measurements_per_depth_-_step_size_{step_size}'
+        if use_log_scale:
+            plot_name += '_-_log_scale'
+        file = measurements.plot.constants.PLOT_FILE.format(
+            tracer=measurements_object.tracer,
+            data_set=measurements_object.data_set_name,
+            kind='number_of_measurements',
+            kind_id=str(measurements_object.sample_lsm),
+            plot_name=plot_name)
+
+    points = measurements_object.points
+    assert points.ndim == 2
+    assert points.shape[1] == 4
+    z = points[:, 3]
+
+    util.plot.histogram(file, z, step_size=step_size, use_log_scale=use_log_scale, overwrite=overwrite)
+
+
+def per_space(measurements_object, use_log_scale=True, file=None, overwrite=False):
+    if file is None:
+        plot_name = 'number_of_measurements_per_space'
+        if use_log_scale:
+            plot_name += '_-_log_scale'
+        file = measurements.plot.constants.PLOT_FILE.format(
+            tracer=measurements_object.tracer,
+            data_set=measurements_object.data_set_name,
+            kind='number_of_measurements',
+            kind_id=str(measurements_object.sample_lsm),
+            plot_name=plot_name)
+
+    points = measurements_object.points
+    assert points.ndim == 2
+    assert points.shape[1] == 4
+    space_points = points[:, 1:]
+    lsm = measurements_object.sample_lsm
+    space_coordinates = lsm.coordinates_to_map_indices(space_points, int_indices=True)
+    data, counts = np.unique(space_coordinates, axis=0, return_counts=True)
+    data = np.concatenate((data, counts[:, np.newaxis]), axis=1)
+    data = lsm.insert_index_values_in_map(data, no_data_value=np.inf)
+
+    util.plot.data(file, data, no_data_value=np.inf, use_log_scale=use_log_scale, contours=False, colorbar=True, overwrite=overwrite)
