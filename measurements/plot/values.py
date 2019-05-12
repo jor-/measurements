4dbac0212a8e9891ba49967074eaa8f929267170
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-05-12 14:58:39 +0200

    ENH: measurements.plot.values: histogram_and_density_function: kde_linewidth keyword added

--- measurements/plot/values.py
+++ measurements/plot/values.py
@@ -7,7 +7,7 @@ import measurements.universal.dict
 import measurements.plot.util
 
 
-def histogram_and_density_function(measurements_object, file=None, min_number_of_values=50, histogram_step_size=0.05, type='measurement_results', overwrite=False, **kwargs):
+def histogram_and_density_function(measurements_object, file=None, min_number_of_values=50, histogram_step_size=0.05, kde_linewidth=3, type='measurement_results', overwrite=False, **kwargs):
     # check input
     SUPPORTED_TYPES = ('measurement_results', 'true_concentrations')
     if type not in SUPPORTED_TYPES:
@@ -25,7 +25,7 @@ def histogram_and_density_function(measurements_object, file=None, min_number_of
 
     # prepare file name
     kind = os.path.join('data', type)
-    kind_id = os.path.join(str(measurements_object.sample_lsm), 'histogram_and_density_function', f'step_size_{histogram_step_size}', f'min_values_{min_number_of_values}')
+    kind_id = os.path.join(str(measurements_object.sample_lsm), 'histogram_and_density_function', f'kde_linewidth_{kde_linewidth}', f'histogram_step_size_{histogram_step_size}', f'min_values_{min_number_of_values}')
     plot_name = 'histogram_and_density_{point}'
     file = measurements.plot.util.filename(measurements_object, kind, kind_id, plot_name)
 
@@ -35,6 +35,6 @@ def histogram_and_density_function(measurements_object, file=None, min_number_of
         x_min = np.floor(min(values) * 2) / 2
         x_max = np.ceil(max(values) * 2) / 2
         point_str = ','.join(map(str, point))
-        util.plot.save.histogram(file.format(point=point_str), values, step_size=histogram_step_size, x_min=x_min, x_max=x_max, density=True, add_kde=True, overwrite=overwrite, **kwargs)
+        util.plot.save.histogram(file.format(point=point_str), values, step_size=histogram_step_size, kde_linewidth=kde_linewidth, x_min=x_min, x_max=x_max, density=True, add_kde=True, overwrite=overwrite, **kwargs)
 
     md.iterate_items(plot_hist)
