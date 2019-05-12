6600f24c7304cc05fec3fc52021d16fd910dc642
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-05-12 11:11:12 +0200

    ENH: measurements.plot.values: histogram_and_density_function: true_concentrations as supported type added

--- measurements/plot/values.py
+++ measurements/plot/values.py
@@ -7,19 +7,31 @@ import measurements.universal.dict
 import measurements.plot.util
 
 
-def histogram_and_density_function(measurements_object, file=None, min_number_of_values=100, histogram_step_size=0.05, overwrite=False, **kwargs):
+def histogram_and_density_function(measurements_object, file=None, min_number_of_values=50, histogram_step_size=0.05, type='measurement_results', overwrite=False, **kwargs):
+    # check input
+    SUPPORTED_TYPES = ('measurement_results', 'true_concentrations')
+    if type not in SUPPORTED_TYPES:
+        raise ValueError(f'Type {type} unknown. Only {SUPPORTED_TYPES} are supported.')
+
+    # prepare data in measurement dict
     md = measurements.universal.dict.MeasurementsDict()
     md.append_values(measurements_object.points, measurements_object.values)
     lsm = measurements_object.sample_lsm
     md.categorize_indices_to_lsm(lsm, discard_year=False)
+    if type == 'true_concentrations':
+        md.means(return_type='self')
+        md.discard_year()
     md.filter_min_number_of_values(min_number_of_values)
 
-    kind = 'data'
+    # prepare file name
+    kind = os.path.join('data', type)
     kind_id = os.path.join(str(measurements_object.sample_lsm), 'histogram_and_density_function', f'step_size_{histogram_step_size}', f'min_values_{min_number_of_values}')
     plot_name = 'histogram_and_density_{point}'
     file = measurements.plot.util.filename(measurements_object, kind, kind_id, plot_name)
 
+    # plot
     def plot_hist(point, values):
+        assert len(values) >= min_number_of_values
         x_min = np.floor(min(values) * 2) / 2
         x_max = np.ceil(max(values) * 2) / 2
         point_str = ','.join(map(str, point))
