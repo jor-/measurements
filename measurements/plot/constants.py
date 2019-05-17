42027da0026101b76a59677d1488eae45a61d41c
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-05-17 10:44:30 +0200

    ENH: measurements.plot: replace '.' in filenames by '_'

--- measurements/plot/constants.py
+++ measurements/plot/constants.py
@@ -11,4 +11,5 @@ MEASUREMENT_DIR = os.path.join(BASE_DIR, 'measurements', '{tracer}', '{data_set}
 KIND_DIR = os.path.join(MEASUREMENT_DIR, '{kind}', '{kind_id}')
 
 PLOT_DEFAULT_FILE_EXTENSION = 'svg'
-PLOT_FILE = os.path.join(KIND_DIR, '{plot_name}.{file_extension}')
+PLOT_FILE_WITHOUT_FILE_EXTENSION = os.path.join(KIND_DIR, '{plot_name}')
+PLOT_FILE = PLOT_FILE_WITHOUT_FILE_EXTENSION + '.{file_extension}'
--- measurements/plot/util.py
+++ measurements/plot/util.py
@@ -1,12 +1,27 @@
+import pathlib
+import warnings
+
 import measurements.plot.constants
 
 
 def filename(measurements_object, kind, kind_id, plot_name):
-    file = measurements.plot.constants.PLOT_FILE.format(
+    file = measurements.plot.constants.PLOT_FILE_WITHOUT_FILE_EXTENSION.format(
         tracer=measurements_object.tracer,
         data_set=measurements_object.data_set_name,
         kind=kind,
         kind_id=kind_id,
-        plot_name=plot_name,
-        file_extension=measurements.plot.constants.PLOT_DEFAULT_FILE_EXTENSION)
+        plot_name=plot_name)
+    # check for bad chars
+    bad_char = '.'
+    replacement_char = '_'
+    if bad_char in file:
+        warnings.warn(f'A "{bad_char}" is in the filename {file} which should be avoided. Replacing "{bad_char}" by "{replacement_char}".')
+        file = file.replace(bad_char, replacement_char)
+    assert bad_char not in file
+    # append file extension
+    file_extension = measurements.plot.constants.PLOT_DEFAULT_FILE_EXTENSION
+    assert not file_extension.startswith('.')
+    file += '.' + file_extension
+    path = pathlib.PurePath(file)
+    # return
     return file
