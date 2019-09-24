cd2aa18e3d8ac162b3dd1c30e5f3d475066d6a08
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-09-24 20:34:15 +0200

    ENH: measurements.plot.data: plot: kargs are excepted now in every plot

--- measurements/plot/data.py
+++ measurements/plot/data.py
@@ -91,7 +91,7 @@ def _average_data(data, sample_lsm, exclude_axes=None):
     return data_averaged
 
 
-def plot_time_space_depth(data, file, v_max=None, overwrite=False, colorbar=True, t_dim=None):
+def plot_time_space_depth(data, file, v_max=None, overwrite=False, colorbar=True, t_dim=None, **kargs):
     assert data.ndim == 4
     data = _change_t_dim(data, t_dim=t_dim)
     v_min = 0
@@ -106,14 +106,14 @@ def plot_time_space_depth(data, file, v_max=None, overwrite=False, colorbar=True
     else:
         file = _append_to_filename(file, '_-_depth_{depth}')
     # plot data
-    util.plot.save.data(file, data, no_data_value=np.inf, v_min=v_min, v_max=v_max, contours=False, colorbar=colorbar, overwrite=overwrite)
+    util.plot.save.data(file, data, no_data_value=np.inf, v_min=v_min, v_max=v_max, contours=False, colorbar=colorbar, overwrite=overwrite, **kargs)
 
 
-def plot_space_depth(data, file, v_max=None, overwrite=False, colorbar=True):
-    plot_time_space_depth(data, file, v_max=v_max, overwrite=overwrite, t_dim=1, colorbar=colorbar)
+def plot_space_depth(data, file, v_max=None, overwrite=False, colorbar=True, **kargs):
+    plot_time_space_depth(data, file, v_max=v_max, overwrite=overwrite, t_dim=1, colorbar=colorbar, **kargs)
 
 
-def plot_depth(data, base_file, sample_lsm, v_max=None, overwrite=False, depth_on_y_axis=True, depth_decimals=None):
+def plot_depth(data, base_file, sample_lsm, v_max=None, overwrite=False, depth_on_y_axis=True, depth_decimals=None, **kargs):
     assert data.ndim == 4
     v_min = 0
     # prepare file
@@ -124,12 +124,12 @@ def plot_depth(data, base_file, sample_lsm, v_max=None, overwrite=False, depth_o
         x = np.arange(len(data_averaged))
         transform_depth_ticks = lambda ticks: _transform_depth_ticks(ticks, sample_lsm, ticks_decimals=depth_decimals)
         if depth_on_y_axis:
-            util.plot.save.fill_between_x(file, x, 0, data_averaged, y_min=x.min(), y_max=x.max(), x_min=v_min, x_max=v_max, color='b', overwrite=overwrite, transform_y=transform_depth_ticks, invert_y_axis=True)
+            util.plot.save.fill_between_x(file, x, 0, data_averaged, y_min=x.min(), y_max=x.max(), x_min=v_min, x_max=v_max, color='b', overwrite=overwrite, transform_y=transform_depth_ticks, invert_y_axis=True, **kargs)
         else:
-            util.plot.save.fill_between(file, x, 0, data_averaged, x_min=x.min(), x_max=x.max(), y_min=v_min, y_max=v_max, color='b', overwrite=overwrite, transform_x=transform_depth_ticks)
+            util.plot.save.fill_between(file, x, 0, data_averaged, x_min=x.min(), x_max=x.max(), y_min=v_min, y_max=v_max, color='b', overwrite=overwrite, transform_x=transform_depth_ticks, **kargs)
 
 
-def plot_time(data, base_file, sample_lsm, v_max=None, overwrite=False):
+def plot_time(data, base_file, sample_lsm, v_max=None, overwrite=False, **kargs):
     assert data.ndim == 4
     v_min = 0
     # prepare file
@@ -145,10 +145,10 @@ def plot_time(data, base_file, sample_lsm, v_max=None, overwrite=False):
         if v_max is None:
             v_max = util.plot.auxiliary.v_max(data_averaged)
         # plot
-        util.plot.save.line(file, x, data_averaged, y_min=v_min, y_max=v_max, line_color='b', line_width=3, xticks=np.arange(5) / 4, overwrite=overwrite)
+        util.plot.save.line(file, x, data_averaged, y_min=v_min, y_max=v_max, line_color='b', line_width=3, xticks=np.arange(5) / 4, overwrite=overwrite, **kargs)
 
 
-def plot_histogram(data, base_file, v_max=None, overwrite=False):
+def plot_histogram(data, base_file, v_max=None, overwrite=False, **kargs):
     assert data.ndim == 4
     v_min = 0
     # prepare file
@@ -158,7 +158,7 @@ def plot_histogram(data, base_file, v_max=None, overwrite=False):
         data_non_nan = data[~np.isnan(data)]
         if v_max is None:
             v_max = util.plot.auxiliary.v_max(data_non_nan)
-        util.plot.save.histogram(file, data_non_nan, x_min=v_min, x_max=v_max, use_log_scale=True, overwrite=overwrite)
+        util.plot.save.histogram(file, data_non_nan, x_min=v_min, x_max=v_max, use_log_scale=True, overwrite=overwrite, **kargs)
 
 
 def _prepare_tick_lables(tick_lables, ticks_decimals=None):
@@ -241,38 +241,39 @@ def plot_y_z_profile(data, base_file, sample_lsm, v_max=None, x_coordinate_from=
 
 def plot(data, base_file, sample_lsm, plot_type='all', v_max=None, overwrite=False, **kargs):
     if plot_type == 'all':
-        plot_space_depth(data, base_file, v_max=v_max, overwrite=overwrite)
-        plot_depth(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite)
-        plot_time(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite)
-        plot_histogram(data, base_file, v_max=v_max, overwrite=overwrite)
-        plot_y_z_profile(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite, x_coordinate_from=None, x_coordinate_to=None)
-        plot_y_z_profile(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite, x_coordinate_from=125, x_coordinate_to=290)
-        plot_y_z_profile(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite, x_coordinate_from=290, x_coordinate_to=20)
+        plot_time_space_depth(data, base_file, v_max=v_max, overwrite=overwrite, **kargs)
+        plot_space_depth(data, base_file, v_max=v_max, overwrite=overwrite, **kargs)
+        plot_depth(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite, **kargs)
+        plot_time(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite, **kargs)
+        plot_histogram(data, base_file, v_max=v_max, overwrite=overwrite, **kargs)
+        plot_y_z_profile(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite, x_coordinate_from=None, x_coordinate_to=None, **kargs)
+        plot_y_z_profile(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite, x_coordinate_from=125, x_coordinate_to=290, **kargs)
+        plot_y_z_profile(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite, x_coordinate_from=290, x_coordinate_to=20, **kargs)
         if v_max is None:
             # plot with fixed v_max
             fixed_base_file = _append_v_max_to_filename(base_file, 'fixed')
-            plot_time_space_depth(data, fixed_base_file, v_max='fixed', overwrite=overwrite)
-            plot_space_depth(data, fixed_base_file, v_max='fixed', overwrite=overwrite)
+            plot_time_space_depth(data, fixed_base_file, v_max='fixed', overwrite=overwrite, **kargs)
+            plot_space_depth(data, fixed_base_file, v_max='fixed', overwrite=overwrite, **kargs)
             # plot seasonal values
             if data.ndim == 4:
                 t_dim = data.shape[0]
                 if t_dim % 4 == 0:
                     seasonal_data = _change_t_dim(data, t_dim=4)
-                    plot_time_space_depth(seasonal_data, base_file, v_max=None, overwrite=overwrite)
-                    plot_time_space_depth(seasonal_data, base_file, v_max=None, overwrite=overwrite, colorbar=False)
-                    plot_time_space_depth(seasonal_data, fixed_base_file, v_max='fixed', overwrite=overwrite)
-                    plot_time_space_depth(seasonal_data, fixed_base_file, v_max='fixed', overwrite=overwrite, colorbar=False)
+                    plot_time_space_depth(seasonal_data, base_file, v_max=None, overwrite=overwrite, **kargs)
+                    plot_time_space_depth(seasonal_data, base_file, v_max=None, overwrite=overwrite, colorbar=False, **kargs)
+                    plot_time_space_depth(seasonal_data, fixed_base_file, v_max='fixed', overwrite=overwrite, **kargs)
+                    plot_time_space_depth(seasonal_data, fixed_base_file, v_max='fixed', overwrite=overwrite, colorbar=False, **kargs)
             # plot time difference
             diff = _data_abs_time_difference(data)
             diff_base_file = _append_to_filename(base_file, '_-_abs_time_diff')
-            plot_space_depth(diff, diff_base_file, v_max=None, overwrite=overwrite)
-            plot_depth(diff, diff_base_file, sample_lsm, v_max=None, overwrite=overwrite)
+            plot_space_depth(diff, diff_base_file, v_max=None, overwrite=overwrite, **kargs)
+            plot_depth(diff, diff_base_file, sample_lsm, v_max=None, overwrite=overwrite, **kargs)
             fixed_diff_base_file = _append_v_max_to_filename(diff_base_file, 'fixed')
-            plot_space_depth(diff, fixed_diff_base_file, v_max='fixed', overwrite=overwrite)
+            plot_space_depth(diff, fixed_diff_base_file, v_max='fixed', overwrite=overwrite, **kargs)
             # plot depth difference
             diff = _data_abs_depth_difference(data)
             diff_base_file = _append_to_filename(base_file, '_-_abs_depth_diff')
-            plot_depth(diff, diff_base_file, sample_lsm, v_max=None, overwrite=overwrite)
+            plot_depth(diff, diff_base_file, sample_lsm, v_max=None, overwrite=overwrite, **kargs)
     elif plot_type == 'time_space_depth':
         plot_time_space_depth(data, base_file, v_max=v_max, overwrite=overwrite, **kargs)
     elif plot_type == 'space_depth':
@@ -296,8 +297,6 @@ def plot(data, base_file, sample_lsm, plot_type='all', v_max=None, overwrite=Fal
         diff = _data_abs_depth_difference(data)
         diff_base_file = _append_to_filename(base_file, '_-_abs_depth_diff')
         plot_depth(diff, diff_base_file, sample_lsm, v_max=None, overwrite=overwrite, **kargs)
-    elif plot_type == 'plot_y_z_profile':
-        plot_y_z_profile(data, base_file, sample_lsm, v_max=v_max, overwrite=overwrite)
     else:
         raise ValueError(f'Unknown plot type {plot_type}.')
 
@@ -463,7 +462,7 @@ def concentration_quartile_coefficient_of_dispersion_for_sample_lsm(measurements
 
 # *** correlation *** #
 
-def correlation_histogram(measurements_object, file=None, use_abs=False, use_sample_correlation=False, overwrite=False):
+def correlation_histogram(measurements_object, file=None, use_abs=False, use_sample_correlation=False, overwrite=False, **kargs):
     if file is None:
         if use_sample_correlation:
             kind_id = measurements_object.sample_correlation_id
@@ -501,10 +500,10 @@ def correlation_histogram(measurements_object, file=None, use_abs=False, use_sam
                     tick_number = 3
                 else:
                     tick_number = 5
-            util.plot.save.histogram(file_with_scale, data, step_size=0.05, x_min=x_min, x_max=1, tick_number=tick_number, use_log_scale=use_log_scale)
+            util.plot.save.histogram(file_with_scale, data, step_size=0.05, x_min=x_min, x_max=1, tick_number=tick_number, use_log_scale=use_log_scale, **kargs)
 
 
-def correlation_sparsity_pattern(measurements_object, file=None, permutation_method=None, use_sample_correlation=False, overwrite=False):
+def correlation_sparsity_pattern(measurements_object, file=None, permutation_method=None, use_sample_correlation=False, overwrite=False, **kargs):
     # set permutation method
     permutation_method_decomposition_correlation_old = measurements_object.permutation_method_decomposition_correlation
     if permutation_method is not None:
@@ -532,18 +531,18 @@ def correlation_sparsity_pattern(measurements_object, file=None, permutation_met
         else:
             A = measurements_object.correlations_own
         # plot
-        util.plot.save.sparse_matrix_pattern(file, A, axis_labels=False)
+        util.plot.save.sparse_matrix_pattern(file, A, axis_labels=False, **kargs)
     # plot decomposition matrix sparsity pattern if not existing
     if not use_sample_correlation:
         file = _append_to_filename(file, '_-_L')
         if overwrite or not os.path.exists(file):
             A = measurements_object.correlations_own_decomposition.L
-            util.plot.save.sparse_matrix_pattern(file, A, axis_labels=False)
+            util.plot.save.sparse_matrix_pattern(file, A, axis_labels=False, **kargs)
     # restore permutation method
     measurements_object.permutation_method_decomposition_correlation = permutation_method_decomposition_correlation_old
 
 
-def correlation_and_sample_correlation_sparsity_pattern(measurements_object, file=None, overwrite=False):
+def correlation_and_sample_correlation_sparsity_pattern(measurements_object, file=None, overwrite=False, **kargs):
     # get file name
     if file is None:
         kind = os.path.join('correlation', 'correlation', 'sparsity_pattern')
@@ -562,4 +561,5 @@ def correlation_and_sample_correlation_sparsity_pattern(measurements_object, fil
         util.plot.save.sparse_matrices_patterns_with_differences(
             file, A, B,
             colors=((1, 0, 0), (1, 1, 1), (1, 0, 1), (0, 0, 1)),
-            labels=('removed', 'inserted', 'changed', 'unchanged'),)
+            labels=('removed', 'inserted', 'changed', 'unchanged'),
+            **kargs)
