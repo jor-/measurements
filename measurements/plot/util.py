368211ca6314372f56de2f42cc3979455c200a6c
Joscha Reimer <jor@informatik.uni-kiel.de>
2020-02-05 09:28:49 +0100

    MAINT: measurements.plot.util: use util.plot.auxiliary.replace_bad_characters instead of own implementation

--- measurements/plot/util.py
+++ measurements/plot/util.py
@@ -3,6 +3,7 @@ import warnings
 
 import numpy as np
 
+import util.plot.auxiliary
 import measurements.plot.constants
 
 
@@ -13,13 +14,8 @@ def filename(measurements_object, kind, kind_id, plot_name):
         kind=kind,
         kind_id=kind_id,
         plot_name=plot_name)
-    # check for bad chars
-    bad_char = '.'
-    replacement_char = '_'
-    if bad_char in file:
-        warnings.warn(f'A "{bad_char}" is in the filename {file} which should be avoided. Replacing "{bad_char}" by "{replacement_char}".')
-        file = file.replace(bad_char, replacement_char)
-    assert bad_char not in file
+    # replace bad chars
+    file = util.plot.auxiliary.replace_bad_characters(file)
     # append file extension
     file_extension = measurements.plot.constants.PLOT_DEFAULT_FILE_EXTENSION
     assert not file_extension.startswith('.')
